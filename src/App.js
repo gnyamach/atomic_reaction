f8788fa794e4c9be2054fd6d55cbeadabbd2eea4
gnyamach <gnyamach@msudenver.edu>
2017-11-26 14:21:12 -0700

    Installed React and Node js and cleanined up the files to start the application from scratch

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         The atomic reaction application
+      </div>
+    );
+  }
+}
+
+export default App;
